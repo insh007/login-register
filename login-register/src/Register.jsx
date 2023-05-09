ad1150b00a8a801c41964a2d2539e70d1dc8b6a8
insh007 <aliinshad007@gmail.com>
2023-05-10 02:11:06 +0530

    add components

---
+++ login-register/src/Register.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const Register = (props) => {
+    const [email, setEmail] = useState("")
+    const [pass, setPassword] = useState("")
+    const [name, setName] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // console.log(name)
+    }
+
+    return (
+        <div className='auth-form-container' >
+            <h2>Register</h2>
+            <form className='register-form' onSubmit={handleSubmit}>
+                <label htmlFor="name">Full name</label>
+                <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Jhon Wick' name="name" id="name" />
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='yourgmail@gmail.com' id='email' name='email' />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='********' id='password' name='password' />
+                <button type='submit' >Register</button>
+            </form>
+            <button className='link-btn' onClick={() => props.onFormSwitch('login')} >Already have an account? Login here</button>
+        
+        </div>
+    )
+}
+
+export default Register
